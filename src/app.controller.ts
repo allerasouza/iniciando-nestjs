5ae3bd4f21da59c53194456d8ad27e562bbe75db
Aller Augusto de Souza <allerasouza@gmail.com>
2020-06-29 22:09:15 -0300

    Setup nestjs

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,18 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('prefixo') // /
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get() // /prefixo
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('teste') // /prefixo/teste
+  acao(): string {
+    return 'Full cycle';
+  }
+
 }
